d9b418f0e47476d63f99637f4c851dbb64b8c06b
beckmi <beckmi@c17157d5-08f2-41e7-a131-df055f6a0da3>
2012-03-14 10:13:12 +0000

    Converting to XOOPS 2.5.x Admin GUI

--- tplleaguestats/admin/index.php
+++ tplleaguestats/admin/index.php
@@ -1,42 +1,30 @@
 <?php
-/*
-************************************************************
-TPLLeagueStats is a league stats software designed for football (soccer)
-team.
+/**
+ * Private message
+ *
+ * You may not change or alter any portion of this comment or credits
+ * of supporting developers from this source code or any supporting source code 
+ * which is considered copyrighted (c) material of the original comment or credit authors.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * @copyright       The XOOPS Project http://sourceforge.net/projects/xoops/
+ * @license         http://www.fsf.org/copyleft/gpl.html GNU public license
+ * @package         MRBS
+ * @since           1.41
+ * @author          jobrazo
+ * @version         $Id: admin.php  $
+ */
 
-Copyright (C) 2003  Timo Leppänen / TPL Design
-email:     info@tpl-design.com
-www:       www.tpl-design.com/tplleaguestats
-
-This program is free software; you can redistribute it and/or
-modify it under the terms of the GNU General Public License
-as published by the Free Software Foundation; either version 2
-of the License, or (at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
-
-************************************************************
-Ported to xoops by 
-Mythrandir http://www.web-udvikling.dk
-and 
-ralf57 http://www.madeinbanzi.it
-
-************************************************************
-*/
-
-
-include '../../../include/cp_header.php'; //Include file, which checks for permissions and sets navigation
+require_once dirname(dirname(dirname(dirname(__FILE__)))) . '/include/cp_header.php';
+include_once dirname(__FILE__) . '/admin_header.php';
 
 xoops_cp_header();
 
-header("Location:leaguematches.php");
+	$indexAdmin = new ModuleAdmin();
 
-xoops_cp_footer();
-?>
+    echo $indexAdmin->addNavigation('index.php');
+    echo $indexAdmin->renderIndex();
+
+include "admin_footer.php";
