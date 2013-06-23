36115742ce3bad66ba90b677f12ead76b5e93568
cesag <cesag@c17157d5-08f2-41e7-a131-df055f6a0da3>
2013-06-23 15:27:10 +0000

    Moving toplist, tplleaguestats and twitterbomb non english files to XoopsLanguages folder.

--- tplleaguestats/language/italian/blocks.php
+++
@@ -1,8 +0,0 @@
-<?php
-define("_BL_MINITABLE", 'Miniclassifica');
-define("_BL_TEAM","Squadra");
-define("_BL_GOALS","F-S");
-define("_BL_POINTS","Pti");
-define("_BL_GOTOMAIN","Vai al campionato");
-
-?>
